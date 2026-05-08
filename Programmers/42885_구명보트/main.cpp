ef2cc3ee6174c801b0dbe30c4a9e9d798413e406
YoungJJun <young_879@naver.com>
2026-05-08 17:37:43 +0900

    add: Programmers 42885 solution

---
+++ Programmers/42885_구명보트/main.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+
+    sort(people.begin(), people.end());
+    int i = 0;
+    int j = people.size()-1;
+
+    while(i<=j){
+        if(i==j){
+            answer++;
+            break;
+        }
+
+        if(people[i]+people[j]<=limit){
+            answer++;
+            i++;
+            j--;
+        }
+        else{
+            answer++;
+            j--;
+        }
+    }
+
+
+    return answer;
+}
